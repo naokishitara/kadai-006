b4ba02691e05321c6a39f9fa6e6696803997c21f
設樂直希 <yatta5537@gmail.com>
2022-09-21 12:53:55 +0000

    kadai sub

---
+++ kadai_06.php
@@ -0,0 +1,26 @@
+function kakezan($num){
+    return $num *2;
+}
+echo 
+
+// 課題１番
+
+
+function sum($a,$b){
+   return $a + $b;
+}
+echo
+//2番
+
+function product($arr){
+   $result = 0;
+   for ($i = 0; $i < count($arr);$i++){
+   if($i == 0){
+      $reault += $arr[$i];
+      }else{
+         $result := $arr[$i];
+    }
+      return $result;
+    }
+    
+      
